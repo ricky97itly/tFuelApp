4ce5be580a465036d51d0837095bb05374b2fa1e
riccardomores <riccardo.mores@ied.edu>
2019-07-05 15:07:13 +0200

    UI: Completed calculate page and more
    • Calculate page is completed
    • TabBar is completed
    • Added a pod for textFields

--- tFuel/tFuel/AppDelegate.swift
+++ tFuel/tFuel/AppDelegate.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
         // Override point for customization after application launch.
         return true
     }
--- tFuel/tFuel/CalculateViewController.swift
+++ tFuel/tFuel/CalculateViewController.swift
@@ -9,26 +9,28 @@
 import UIKit
 
 class CalculateViewController: UIViewController {
+    @IBOutlet weak var remainingTF: UITextField!
+    @IBOutlet weak var fuelTF: UITextField!
+    @IBOutlet weak var eurosTF: UITextField!
     @IBOutlet weak var calculateBtn: UIButton!
-    let borderColor: UIColor = UIColor(red: 0.0/255.0, green: 0.37/255.0, blue: 0.41/255.0, alpha: 1.0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        calculateBtn.layer.borderColor = UIColor(red: 0.0/255.0, green: 0.37/255.0, blue: 0.41/255.0, alpha: 1.0).cgColor
-        calculateBtn.layer.borderWidth = 1
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Hide keyboard when I press outside
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
-    */
+    
+    // Hide Keyboard when I press return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        remainingTF.resignFirstResponder()
+        fuelTF.resignFirstResponder()
+        eurosTF.resignFirstResponder()
+        return (true)
+    }
 
 }
