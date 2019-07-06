cb36c371f1561dcc9f47878cc905d1432816b6a9
riccardomores <riccardo.mores@ied.edu>
2019-07-06 14:47:39 +0200

    UI: Login and register

---
+++ tFuel/tFuel/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  
+//
+//  Created by Riccardo Mores on 06/07/2019.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var mailTF: UITextField!
+    @IBOutlet weak var passswordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ tFuel/tFuel/RegisterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewController.swift
+//  
+//
+//  Created by Riccardo Mores on 06/07/2019.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var surnameTF: UITextField!
+    @IBOutlet weak var mailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
